7dfb7ca3e840c487487f0a1ee88f1ab8f9126345
Paulojoserc <pjrc83@gmail.com>
2024-03-09 06:19:28 -0300

    Criação do Services

--- src/main/java/com/devsuperior/movieflix/repositories/ReviewRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/ReviewRepository.java
@@ -1,5 +1,9 @@
 package com.devsuperior.movieflix.repositories;
 
-public interface ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.movieflix.entities.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 
 }
